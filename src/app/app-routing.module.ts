7e0fec0ff17915cc9ec34b33d085d6b27498965c
Adelin Bota <adebota02@gmail.com>
2023-04-28 17:10:22 +0200

    Sin pruebas firebase ni sin base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ClientesComponent } from './clientes/clientes.component';
-import { ProductosComponent } from './productos/productos.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { EditarProductoComponent } from './editar-producto/editar-producto.component';
-import { ClienteComponent } from './cliente/cliente.component';
 import { ErrorPaginaComponent } from './error-pagina/error-pagina.component';
 import { LoginComponent } from './login/login.component';
 import { VerProductosComponent } from './componentes_crud/ver-productos/ver-productos.component';
@@ -16,14 +12,9 @@ import { ServiciosComponent } from './servicios/servicios.component';
 
 const rutas: Routes = [
   {path: '', component: InicioComponent},
-  {path: 'clientes', component: ClientesComponent},
-  {path: 'productos', component: ProductosComponent},
-  {path: 'productos/:categoria', component: ProductosComponent},
   {path: 'contacto', component: ContactoComponent},
   {path: 'citas', component: CitasComponent},
   {path: 'servicios', component: ServiciosComponent},
-  {path: 'producto/:id', component: EditarProductoComponent},
-  {path: 'cliente/:id', component: ClienteComponent},
   {path: 'lista-productos', component: VerProductosComponent},
   {path: 'add-productos', component: AnadirProductoComponent},
   {path: 'actualizar-productos/:idProducto', component: ActualizarProductoComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClientesComponent } from './clientes/clientes.component';
-import { ProductosComponent } from './productos/productos.component';
 import { ContactoComponent } from './contacto/contacto.component';
 import { InicioComponent } from './inicio/inicio.component';
-import { EditarProductoComponent } from './editar-producto/editar-producto.component';
-import { ClienteComponent } from './cliente/cliente.component';
 import { ErrorPaginaComponent } from './error-pagina/error-pagina.component';
-import { ClienteService } from './cliente.service';
-import { ProductoService } from './producto.service';
-import { DatosService } from './datos.service';
 import { LoginComponent } from './login/login.component';
 import { VerProductosComponent } from './componentes_crud/ver-productos/ver-productos.component';
 import { AnadirProductoComponent } from './componentes_crud/anadir-producto/anadir-producto.component';
@@ -29,12 +22,8 @@ import { CitasComponent } from './citas/citas.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ClientesComponent,
-    ProductosComponent,
     ContactoComponent,
     InicioComponent,
-    EditarProductoComponent,
-    ClienteComponent,
     ErrorPaginaComponent,
     LoginComponent,
     VerProductosComponent,
@@ -54,7 +43,7 @@ import { CitasComponent } from './citas/citas.component';
     BrowserAnimationsModule,
 
   ],
-  providers: [ClienteService, ProductoService, DatosService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
